94f694380d980742874b89bd4c7461d3ed5a1ada
PinWei.Wan <wanpinwei@outlook.com>
2021-10-07 11:50:30 +0800

    AsynchronousServerSocketChannel

---
+++ java-nio2/src/test/java/net/java/nio2/async/AsyncEchoIntegrationTest.java
@@ -0,0 +1,36 @@
+package net.java.nio2.async;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AsyncEchoIntegrationTest {
+    private Process server;
+    private AsyncEchoClient client;
+
+    @BeforeAll
+    public void setup() throws IOException, InterruptedException {
+        server = AsyncEchoServer2.start();
+        client = AsyncEchoClient.getInstance();
+    }
+
+    @Test
+    public void givenServerClient_whenServerEchosMessage_thenCorrect() throws Exception {
+        String resp1 = client.sendMessage("hello");
+        String resp2 = client.sendMessage("world");
+        assertEquals("hello", resp1);
+        assertEquals("world", resp2);
+    }
+
+    @AfterAll
+    public void teardown() throws IOException {
+        client.stop();
+        server.destroy();   
+    }
+}
